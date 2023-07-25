44fe2ebbec13a195df1b21e4a1c7b9e8f4a695cc
Peter <peterng.918@gmail.com>
2023-07-25 18:27:04 -0400

    fixed time translation issues

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,16 +1,82 @@
-import React from "react";
+import React, { useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-const Calendar = () => {
+const Calendar = ({ notes }) => {
+  const cal = useRef();
+  const dateRef = useRef();
+  function prevMonth() {
+    const api = cal.current.getApi();
+    api.prev();
+  }
+  function nextMonth() {
+    const api = cal.current.getApi();
+    api.next();
+  }
+  function addEvent() {
+    const api = cal.current.getApi();
+    console.log(dateRef.current.value);
+    api.addEvent({ title: "event 3", date: dateRef.current.value });
+  }
+
+  if (notes) {
+    console.log("testttt,", notes);
+  }
   return (
     <div>
+      <button
+        onClick={() => {
+          prevMonth();
+        }}
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => {
+          nextMonth();
+        }}
+      >
+        next
+      </button>
+      <button
+        onClick={() => {
+          addEvent();
+        }}
+      >
+        Add
+      </button>
+      <input ref={dateRef} type={"date"}></input>
+      {/* will need to figure out why the date is wrongly translated */}
       <FullCalendar
+        ref={cal}
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
-        events={[
-          { title: "event 1", date: "2023-07-01" },
-          { title: "event 2", date: "2023-07-02" },
-        ]}
+        events={
+          notes &&
+          notes.map((note) => {
+            // const date = new Date(note.date);
+            const time = note.date.slice(0, 10);
+            // console.log(note.date.slice(0, 10));
+            // console.log(date);
+            // console.log(date.toDateString());
+            // console.log(
+            //   `${date.getFullYear()}-${date
+            //     .getMonth()
+            //     .toString()
+            //     .padStart(2, "0")}-${date.getDay().toString().padStart(2, "0")}`
+            // );
+            return {
+              // date: `${date.getFullYear()}-${date
+              //   .getMonth()
+              //   .toString()
+              //   .padStart(2, "0")}-${date
+              //   .getDay()
+              //   .toString()
+              //   .padStart(2, "0")}`,
+              date: time,
+              title: note.foods,
+            };
+          })
+        }
       />
       HI
     </div>
